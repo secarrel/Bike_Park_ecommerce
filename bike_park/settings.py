3cd15072b30e72355b4bf54f062cb1e0981bf0ac
secarrel <secarrel@outlook.com>
2024-03-29 12:02:08 +0000

    Add: allauth and settup media, static, gitignore

--- bike_park/settings.py
+++ bike_park/settings.py
@@ -11,17 +11,15 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 import environ 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# reading .env file
-env = environ.Env()
-environ.Env.read_env()
-
-# secret key
-SECRET_KEY = env("SECRET_KEY")
+# Secret Key
+load_dotenv()
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
