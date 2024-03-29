730fe84850d551b2d7dba7e9fc66266dd8130767
secarrel <secarrel@outlook.com>
2024-03-29 12:41:21 +0000

    Add: basket app with views, urls and templates

---
+++ basket/contexts.py
@@ -0,0 +1,36 @@
+from timeslots.models import Timeslot
+from django.shortcuts import get_object_or_404
+
+def basket_contents(request):
+    basket_items = []
+    total = 0 
+    basket = request.session.get('basket', {})
+    
+    activity_ids = set()
+    timeslot_ids = set()
+
+    for item_id, quantity in basket.items():
+        timeslot = get_object_or_404(Timeslot, pk=item_id)
+        activity = timeslot.activity
+        total += quantity * float(activity.price)
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'activity': activity,
+            'timeslot': timeslot
+        })
+
+        activity_ids.add(activity.id)
+        timeslot_ids.add(timeslot.id)
+
+    activity_count = len(activity_ids)
+    timeslot_count = len(timeslot_ids)
+
+    context = {
+        'basket_items': basket_items,
+        'activity_count': activity_count,
+        'timeslot_count': timeslot_count,
+        'total': total,
+    }
+
+    return context
---
+++ basket/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.view_basket, name='view_basket'),
+    path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('update/<item_id>/', views.update_basket, name='update_basket'),
+    path('remove_from_basket/<item_id>/', views.remove_from_basket, name='remove_from_basket'),
+]
