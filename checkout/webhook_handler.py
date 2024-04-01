fa3209b7b57d02df9adf6962a84f8580a921f328
secarrel <secarrel@outlook.com>
2024-04-01 17:04:41 +0100

    Update: webhook handlers to save profile information

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from timeslots.models import Timeslot
+from profiles.models import UserProfile
 
 import stripe
 
@@ -39,6 +40,21 @@ class StripeWH_Handler:
         billing_details = stripe_charge.billing_details
         total = round(stripe_charge.amount / 100, 2)
 
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = billing_details.phone
+                profile.default_street_address1 = billing_details.address.line1
+                profile.default_street_address2 = billing_details.address.line2
+                profile.default_town_or_city = billing_details.address.city
+                profile.default_county = billing_details.address.state
+                profile.default_country = billing_details.address.country
+                profile.default_postcode = billing_details.address.postal_code
+                profile.save()
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -47,12 +63,12 @@ class StripeWH_Handler:
                     full_name__iexact=billing_details.name,
                     email__iexact=billing_details.email,
                     phone_number__iexact=billing_details.phone,
-                    country__iexact=billing_details.address.country,
-                    postcode__iexact=billing_details.address.postal_code,
-                    town_or_city__iexact=billing_details.address.city,
                     street_address1__iexact=billing_details.address.line1,
                     street_address2__iexact=billing_details.address.line2,
+                    town_or_city__iexact=billing_details.address.city,
                     county__iexact=billing_details.address.state,
+                    country__iexact=billing_details.address.country,
+                    postcode__iexact=billing_details.address.postal_code,
                     total=total,
                     original_basket=basket,
                     stripe_pid=pid,
@@ -71,14 +87,15 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=billing_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=billing_details.phone,
-                    country=billing_details.address.country,
-                    postcode=billing_details.address.postal_code,
-                    town_or_city=billing_details.address.city,
                     street_address1=billing_details.address.line1,
                     street_address2=billing_details.address.line2,
+                    town_or_city=billing_details.address.city,
                     county=billing_details.address.state,
+                    country=billing_details.address.country,
+                    postcode=billing_details.address.postal_code,
                     original_basket=basket,
                     stripe_pid=pid,
                 )
