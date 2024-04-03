d5fae86a68dedb303f0fdeeee26f6781f424132a
secarrel <secarrel@outlook.com>
2024-04-03 15:32:01 +0100

    Update: order history to show upcoming and previous bookings

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<int:user_id>/', views.order_history, name='order_history'),
     path('user_details/', views.user_details, name='user_details'),
+    path('order_details/<order_number>', views.order_details, name='order_details'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -66,12 +66,71 @@ def user_details(request):
     return render(request, template, context)
 
 def order_history(request, user_id):
-    # Retrieve the user's orders based on the user_id
-    orders = Order.objects.filter(user_profile__user_id=user_id)
+    """ Display the user's order history. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    orders = profile.orders.all()
+
+    # Dictionary to store timeslots and their quantities
+    timeslot_past = {}
+    timeslot_future = {}
+
+    # Iterate through orders
+    for order in orders:
+        # Iterate through items in the order
+        for item in order.lineitems.all():
+            timeslot = item.timeslot
+            quantity = item.quantity
+            start_time = timeslot.start_time
+
+            # Check timeslot is in the future
+            if start_time < timezone.now():
+
+                # Check if the timeslot is already in the dictionary
+                if timeslot in timeslot_past:
+                    # Increment the quantity if the timeslot exists
+                    timeslot_past[timeslot] += quantity
+                else:
+                    # Add the timeslot to the dictionary with the initial quantity
+                    timeslot_past[timeslot] = quantity
+
+            # Check timeslot is in the future
+            if start_time > timezone.now():
+
+                # Check if the timeslot is already in the dictionary
+                if timeslot in timeslot_future:
+                    # Increment the quantity if the timeslot exists
+                    timeslot_future[timeslot] += quantity
+                else:
+                    # Add the timeslot to the dictionary with the initial quantity
+                    timeslot_future[timeslot] = quantity
+
+
+    # Sort timeslot_quantities by start time
+    sorted_timeslot_past = dict(sorted(timeslot_past.items(), key=lambda item: item[0].start_time, reverse=True))
+    sorted_timeslot_future = dict(sorted(timeslot_future.items(), key=lambda item: item[0].start_time))
 
     template = 'profiles/order_history.html'
     context = {
         'orders': orders,
+        'sorted_timeslot_future': sorted_timeslot_future,
+        'sorted_timeslot_past': sorted_timeslot_past,
+    }
+
+    return render(request, template, context)
+
+
+def order_details(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
