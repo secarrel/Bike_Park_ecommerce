1fd8843ca31c545abce2ff58642b74a80c75e66e
secarrel <secarrel@outlook.com>
2024-04-03 17:02:26 +0100

    Update: activity display to show category specific information

--- activities/urls.py
+++ activities/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_activities, name='activities'),
-    path('<activity_id>', views.activity_details, name='activity_details')
+    path('<int:activity_id>/', views.activity_details, name='activity_details')
 ]
