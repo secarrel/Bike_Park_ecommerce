5009dacf7459fe8f5af625a46660b2cde72807a1
secarrel <secarrel@outlook.com>
2024-04-05 15:57:38 +0100

    Add: edit activity functionality

--- activities/urls.py
+++ activities/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
     path('', views.all_activities, name='activities'),
     path('<int:activity_id>/', views.activity_details, name='activity_details'),
     path('manage_activities/', views.manage_activities, name='manage_activities'),
+    path('manage_timeslots/', views.manage_timeslots, name='manage_timeslots'),
     path('add_activity/', views.add_activity, name='add_activity'),
     path('add_timeslot/', views.add_timeslot, name='add_timeslot'),
+    path('edit_activity/<int:activity_id>/', views.edit_activity, name='edit_activity'),
+    path('edit_timeslot/<int:timeslot_id>/', views.edit_timeslot, name='edit_timeslot'),
 
 ]
--- activities/views.py
+++ activities/views.py
@@ -69,8 +69,19 @@ def activity_details(request, activity_id):
 
 def manage_activities(request):
     """ Show manage activities options """
+    activities = Activity.objects.all()
+    
+    context = {
+        'activities': activities,
+    }
 
-    return render(request, 'activities/manage_activities.html')
+    return render(request, 'activities/manage_activities.html', context)
+
+def manage_timeslots(request):
+    """ Show manage timeslots options """
+
+    return render(request, 'activities/manage_timeslots.html')
+
 
 def add_activity(request):
     """ Add a activity to a catgeory """
@@ -109,3 +120,42 @@ def add_timeslot(request):
     }
 
     return render(request, 'activities/add_timeslot.html', context)
+
+def edit_activity(request, activity_id):
+    """ Edit an activity """
+    activity = get_object_or_404(Activity, pk=activity_id)
+    if request.method == "POST":
+        form = ActivityForm(request.POST, request.FILES, instance=activity)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated activity!')
+            return redirect(reverse('activity_details', args=[activity.id]))
+        else: 
+            messages.error(request, 'Failed to update activity. Please ensure the form is valid.')
+    else:
+        form = ActivityForm(instance=activity)
+        messages.info(request, f'You are editing {activity.name}')
+
+    template = 'activities/edit_activity.html'
+    context = {
+        'form': form,
+        'activity': activity,
+    }
+
+    return render(request, template, context)
+
+
+def edit_timeslot(request, timeslot_id):
+    """ Edit an activity """
+
+    timeslot = get_object_or_404(Activity, pk=timeslot_id)
+    form = TimeslotForm(instance=timeslot)
+    messages.info(request, f'You are editing {timeslot.name}')
+
+    template = 'activities/edit_timeslot.html'
+    context = {
+        'form': form,
+        'timeslot': timeslot,
+    }
+
+    return render(request, template, context)
