12c3e0eb527dc75f51747665a37b45cb59210abb
secarrel <secarrel@outlook.com>
2024-04-06 11:46:37 +0100

    Add: heroku procfile and relevant variables to connect to elephantSQL and heroku

--- bike_park/settings.py
+++ bike_park/settings.py
@@ -1,4 +1,6 @@
 import os
+import dj_database_url
+
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -12,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # debug
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fasttrack-bike-park-415cb30571a3.herokuapp.com', 'localhost']
 
 # Application definition
 INSTALLED_APPS = [
@@ -98,12 +100,17 @@ WSGI_APPLICATION = 'bike_park.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
