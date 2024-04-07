420eff79385e9aa64032b92c54aca3470e788915
secarrel <secarrel@outlook.com>
2024-04-07 14:45:33 +0100

    Update: settings to set up emails

--- bike_park/settings.py
+++ bike_park/settings.py
@@ -186,7 +186,6 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'ftbikepark@info.com'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
