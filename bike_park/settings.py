08733d639a58952a104a5e0cdce9cf9a38126047
secarrel <secarrel@outlook.com>
2024-04-11 11:01:25 +0100

    Update: email configuration

--- bike_park/settings.py
+++ bike_park/settings.py
@@ -188,4 +188,4 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = 'fasttrack@bikepark.com'
