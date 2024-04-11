1db8872885f38da1d64f647e1a4343f1d02f3a7c
secarrel <secarrel@outlook.com>
2024-04-11 15:31:22 +0100

    Update: code in timeslots app for PEP8 complience

--- timeslots/models.py
+++ timeslots/models.py
@@ -29,17 +29,19 @@ class Timeslot(models.Model):
         if self.available_capacity in [None, '']:
             if self.activity:
                 self.available_capacity = self.activity.capacity
-        
+
         super().save(*args, **kwargs)
 
-# Add a notes section in the model above which will translate to be 'Description' in the 'modal' of scheduler.
+# Add a notes section in the model above which will translate to be
+# 'Description' in the 'modal' of scheduler.
 # Add something like this to signals.py:
 # @receiver(post_save, sender=Timeslot)
 # def add_timeslot_to_scheduler(sender, instance, created, **kwargs):
 #     if created:  # Only add to scheduler if the timeslot is newly created
 #         # Create a new event in the scheduler
 #         event = Event.objects.create(
-#             title=instance.title,  # Adjust these fields based on your Timeslot model
+#             title=instance.title,  # Adjust these fields
+#                                      based on your Timeslot model
 #             start=instance.start_time,
 #             end=instance.end_time,
 #             description=instance.description,
@@ -48,10 +50,10 @@ class Timeslot(models.Model):
 #         # Save the event
 #         event.save()
 # This will need all the fields in the event model.
-# Create rules of daily, weekly, monthly, hourly etc. 
+# Create rules of daily, weekly, monthly, hourly etc.
         # Allow these options as possible rules
         # Then allow the user to add a end point for the rule
 
 
 # Practice displaying the calander on a seperate page until it works properly.
-        # Make sure urls and views are set up correctly 
+        # Make sure urls and views are set up correctly
