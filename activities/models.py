d5a7029533f3078b7e5ecea8fe928e1dd09836bd
secarrel <secarrel@outlook.com>
2024-04-11 10:01:50 +0100

    Update: models to remove image url field from database

--- activities/models.py
+++ activities/models.py
@@ -14,6 +14,7 @@ class Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     overview = models.TextField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = CloudinaryField(
         'category',
         folder='categories',
@@ -66,6 +67,7 @@ class Activity(models.Model):
     requirements = models.TextField()
     details = models.TextField()
     duration = models.DurationField(default=timedelta)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = CloudinaryField(
         'activity',
         folder='activities',
--- bike_park/settings.py
+++ bike_park/settings.py
@@ -162,7 +162,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
