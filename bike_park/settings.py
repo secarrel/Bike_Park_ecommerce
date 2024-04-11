c49f86a529b696aeca2b9226358f6c51f368798c
secarrel <secarrel@outlook.com>
2024-04-11 10:26:31 +0100

    Update: static root settings

--- bike_park/settings.py
+++ bike_park/settings.py
@@ -159,6 +159,7 @@ cloudinary.config(
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
