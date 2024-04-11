155ff914c15fa07a4aaa735d64ba7fd7bd60b9cd
secarrel <secarrel@outlook.com>
2024-04-11 10:28:11 +0100

    Update: static file settings

--- bike_park/settings.py
+++ bike_park/settings.py
@@ -161,9 +161,6 @@ cloudinary.config(
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
