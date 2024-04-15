7a3330b1feff12aa805d9fbc926b1eb56e80febb
secarrel <secarrel@outlook.com>
2024-04-15 09:47:47 +0100

    Update: checkout view so admin cannot checkout

--- checkout/views.py
+++ checkout/views.py
@@ -33,100 +33,106 @@ def cache_checkout_data(request):
 
 
 def checkout(request):
-    # Sets Stripe Keys
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    if not request.user.is_superuser:
+        # Sets Stripe Keys
+        stripe_public_key = settings.STRIPE_PUBLIC_KEY
+        stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    if request.method == 'POST':
-        basket = request.session.get('basket', {})
+        if request.method == 'POST':
+            basket = request.session.get('basket', {})
 
-        form_data = {
-            'full_name': request.POST['full_name'],
-            'email': request.POST['email'],
-            'phone_number': request.POST['phone_number'],
-            'country': request.POST['country'],
-            'postcode': request.POST['postcode'],
-            'town_or_city': request.POST['town_or_city'],
-            'street_address1': request.POST['street_address1'],
-            'street_address2': request.POST['street_address2'],
-            'county': request.POST['county'],
-        }
-        order_form = OrderForm(form_data)
-        if order_form.is_valid():
-            order = order_form.save(commit=False)
-            pid = request.POST.get('client_secret').split('_secret')[0]
-            order.stripe_pid = pid
-            order.original_basket = json.dumps(basket)
-            order.save()
-            for item_id, item_data in basket.items():
+            form_data = {
+                'full_name': request.POST['full_name'],
+                'email': request.POST['email'],
+                'phone_number': request.POST['phone_number'],
+                'country': request.POST['country'],
+                'postcode': request.POST['postcode'],
+                'town_or_city': request.POST['town_or_city'],
+                'street_address1': request.POST['street_address1'],
+                'street_address2': request.POST['street_address2'],
+                'county': request.POST['county'],
+            }
+            order_form = OrderForm(form_data)
+            if order_form.is_valid():
+                order = order_form.save(commit=False)
+                pid = request.POST.get('client_secret').split('_secret')[0]
+                order.stripe_pid = pid
+                order.original_basket = json.dumps(basket)
+                order.save()
+                for item_id, item_data in basket.items():
+                    try:
+                        timeslot = Timeslot.objects.get(id=item_id)
+                        if isinstance(item_data, int):
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                timeslot=timeslot,
+                                updated_quantity=item_data,
+                            )
+                            order_line_item.save()
+                    except Timeslot.DoesNotExist:
+                        messages.error(request, (
+                            "One of the activities in your bag wasn't found"
+                            "in our database. Please call us for assistance."
+                        ))
+                        order.delete()
+                        return redirect(reverse('view_basket'))
+
+                request.session['save_info'] = 'save-info' in request.POST
+                return redirect(reverse(
+                    'checkout_success', args=[order.order_number]))
+            else:
+                messages.error(request, 'There was an error with your form. \
+                            Please double check your information.')
+        else:
+            basket = request.session.get('basket', {})
+            if not basket:
+                messages.error(request, "There's nothing in your basket yet")
+                return redirect(reverse('activities'))
+
+            current_basket = basket_contents(request)
+            total = current_basket['total']
+            stripe_total = round(total * 100)
+            stripe.api_key = stripe_secret_key
+            intent = stripe.PaymentIntent.create(
+                amount=stripe_total,
+                currency=settings.STRIPE_CURRENCY,
+            )
+
+            # Attempt to prefill the form with any info in the user profile.
+            if request.user.is_authenticated:
                 try:
-                    timeslot = Timeslot.objects.get(id=item_id)
-                    if isinstance(item_data, int):
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            timeslot=timeslot,
-                            updated_quantity=item_data,
-                        )
-                        order_line_item.save()
-                except Timeslot.DoesNotExist:
-                    messages.error(request, (
-                        "One of the activities in your bag wasn't found"
-                        "in our database. Please call us for assistance."
-                    ))
-                    order.delete()
-                    return redirect(reverse('view_basket'))
-
-            request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse(
-                'checkout_success', args=[order.order_number]))
-        else:
-            messages.error(request, 'There was an error with your form. \
-                           Please double check your information.')
-    else:
-        basket = request.session.get('basket', {})
-        if not basket:
-            messages.error(request, "There's nothing in your basket yet")
-            return redirect(reverse('activities'))
-
-        current_basket = basket_contents(request)
-        total = current_basket['total']
-        stripe_total = round(total * 100)
-        stripe.api_key = stripe_secret_key
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
-        )
-
-        # Attempt to prefill the form with any info in the user profile.
-        if request.user.is_authenticated:
-            try:
-                profile = UserProfile.objects.get(user=request.user)
-                order_form = OrderForm(initial={
-                    'full_name': profile.default_full_name,
-                    'email': profile.user.email,
-                    'phone_number': profile.default_phone_number,
-                    'street_address1': profile.default_street_address1,
-                    'street_address2': profile.default_street_address2,
-                    'town_or_city': profile.default_town_or_city,
-                    'county': profile.default_county,
-                    'country': profile.default_country,
-                    'postcode': profile.default_postcode,
-                })
-            except UserProfile.DoesNotExist:
+                    profile = UserProfile.objects.get(user=request.user)
+                    order_form = OrderForm(initial={
+                        'full_name': profile.default_full_name,
+                        'email': profile.user.email,
+                        'phone_number': profile.default_phone_number,
+                        'street_address1': profile.default_street_address1,
+                        'street_address2': profile.default_street_address2,
+                        'town_or_city': profile.default_town_or_city,
+                        'county': profile.default_county,
+                        'country': profile.default_country,
+                        'postcode': profile.default_postcode,
+                    })
+                except UserProfile.DoesNotExist:
+                    order_form = OrderForm()
+            else:
                 order_form = OrderForm()
-        else:
-            order_form = OrderForm()
 
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
 
-    template = 'checkout/checkout.html'
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-    }
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
+
+    else:
+        messages.error(
+            request, 'As admin, you are not able to make purchases.')
+        return redirect('profile')
 
     return render(request, template, context)
 
