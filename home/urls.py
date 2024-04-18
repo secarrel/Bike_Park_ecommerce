9ad53b8a26a74c766ec6a78e48823ead19798f9f
secarrel <secarrel@outlook.com>
2024-04-18 10:48:34 +0100

    Add: views and navigation to trails and about pages

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('trails/', views.trails, name='trails'),
 ]
--- home/views.py
+++ home/views.py
@@ -14,4 +14,28 @@ def index(request):
 
     return render(request, template, context)
 
-    return render(request, )
+
+def about(request):
+    """ A view to return the about page """
+
+    categories = Category.objects.all()
+
+    template = 'home/about.html'
+    context = {
+        'categories': categories,
+    }
+
+    return render(request, template, context)
+
+
+def trails(request):
+    """ A view to return the trails page """
+
+    categories = Category.objects.all()
+
+    template = 'home/trails.html'
+    context = {
+        'categories': categories,
+    }
+
+    return render(request, template, context)
